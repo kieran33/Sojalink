e5228f300c756aeffc6f265af753f036d7c850f7
kieran33 <lavole.kieran@gmail.com>
2026-04-15 12:07:26 +0200

    refacto: change file name and add test to check with mysql request if database sojalink_test is used correctly

--- tests/unit/test_launch.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@japa/runner'
-
-test.group('Sanity check', () => {
-  test('Database test is used', async ({ assert }) => {
-    assert.equal(process.env.DB_DATABASE, 'sojalink_test')
-  })
-})
