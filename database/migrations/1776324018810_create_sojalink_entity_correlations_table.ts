3189f266d62091f56d6d86547dedc02a07cb2411
kieran33 <lavole.kieran@gmail.com>
2026-04-20 17:02:05 +0200

    fix: correct the constraint fields and add the foreign key constraint for ‘created_by_event_id’, and creation of a composite constraint

--- database/migrations/1776324018810_create_sojalink_entity_correlations_table.ts
+++ database/migrations/1776324018810_create_sojalink_entity_correlations_table.ts
@@ -6,16 +6,32 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.bigIncrements('id')
-      table.string('source_app').unique().notNullable()
-      table.string('source_entity_type').unique().notNullable()
-      table.string('source_entity_id').unique().notNullable()
-      table.string('target_app').unique().notNullable()
-      table.string('target_entity_type').unique().notNullable()
+      table.string('source_app').notNullable()
+      table.string('source_entity_type').notNullable()
+      table.string('source_entity_id').notNullable()
+      table.string('target_app').notNullable()
+      table.string('target_entity_type').notNullable()
       table.string('target_entity_id').notNullable()
       table.string('correlation_key').notNullable()
-      table.bigInteger('created_by_event_id').notNullable()
+      table
+        .bigInteger('created_by_event_id')
+        .unsigned()
+        .references('id')
+        .inTable('sojalink_events')
+        .notNullable()
       table.datetime('created_at').notNullable()
       table.datetime('updated_at').nullable()
+
+      table.unique(
+        [
+          'source_app',
+          'source_entity_type',
+          'source_entity_id',
+          'target_app',
+          'target_entity_type',
+        ],
+        { indexName: 'sojalink_entity_correlations_unique' }
+      )
     })
   }
 
